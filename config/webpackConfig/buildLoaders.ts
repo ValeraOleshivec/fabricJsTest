bf95cf21deb20ca457df265b26efd7128ed45577
Valeriy Oleshivec <v-oleshivec@inbox.ru>
2024-03-24 12:58:26 +0300

    remove create-react-app, add custom webpack with alias and change ts config

---
+++ config/webpackConfig/buildLoaders.ts
@@ -0,0 +1,37 @@
+import webpack from "webpack";
+import { buildSvgLoader } from "./loaders/buildSvgLoader";
+import { buildCssLoader } from "./loaders/buildCssLoader";
+import { BuildOptions } from "./types/config";
+
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const svgLoader = buildSvgLoader();
+  const babelLoader = {
+    test: /\.(js|ts|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+      },
+    },
+  };
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif)$/i,
+    use: [
+      {
+        loader: "file-loader",
+      },
+    ],
+  };
+
+  const cssLoader = buildCssLoader(isDev);
+
+  // Если не используем тайпскрипт - нужен babel-loader
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
+
+  return [babelLoader, typescriptLoader, cssLoader, svgLoader, fileLoader];
+}
--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,10 +1,9 @@
+import cls from "./Input.module.scss";
+import { classNames } from "shared/utils/classNames/classNames";
+interface InputProps {
+  className?: string;
+}
 
-const Input = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+export const Input = ({ className }: InputProps) => {
+  return <div className={classNames(cls.Input, {}, [])}></div>;
 };
-
-export default Input;
